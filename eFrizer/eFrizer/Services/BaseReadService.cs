43f74014069e806b21dc81b1088dd987d430b67b
Kenan Cosic <kenoocosic@hotmail.com>
2021-08-22 16:39:59 +0200

    Added BaseSevices and Interfaces

---
+++ eFrizer/eFrizer/Services/BaseReadService.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using eFrizer.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace eFrizer.Services
+{
+    
+    public class BaseReadService<T, TDb, TSearch> : IReadService<T, TSearch> where T : class where TDb : class where TSearch : class
+    {
+        public eFrizerContext Context { get; set; }
+        protected readonly IMapper _mapper;
+
+        public BaseReadService(eFrizerContext context, IMapper mapper)
+        {
+            Context = context;
+            _mapper = mapper;
+        }
+
+        public virtual IEnumerable<T> Get(TSearch search = null)
+        {
+            var entity = Context.Set<TDb>();
+
+            var list = entity.ToList();
+            return _mapper.Map<List<T>>(list);
+        }
+
+        public virtual T GetById(int id)
+        {
+            var set = Context.Set<TDb>();
+            var entity = set.Find(id);
+            return _mapper.Map<T>(entity);
+        }
+    }
+    
+}
---
+++ eFrizer/eFrizer/Services/IReadService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eFrizer.Services
+{
+    public interface IReadService<T, TSearch> where T : class where TSearch : class
+    {
+        IEnumerable<T> Get(TSearch search = null);
+        public T GetById(int id);
+    }
+    
+}
