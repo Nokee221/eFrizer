25979bc6d8710da031222c221c636f2af84e685b
Amar Čerim <amarcerim8@gmail.com>
2021-09-20 12:32:27 +0200

    migrations done

--- eFrizer/eFrizer/Program.cs
+++ eFrizer/eFrizer/Program.cs
@@ -1,5 +1,7 @@
+using eFrizer.Database;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +15,13 @@ namespace eFrizer
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var database = scope.ServiceProvider.GetService<eFrizerContext>();
+                new SetupService().Init(database);
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ eFrizer/eFrizer/SetupService.cs
@@ -0,0 +1,24 @@
+﻿using eFrizer.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eFrizer
+{
+    public class SetupService
+    {
+        public void Init(eFrizerContext context)
+        {
+            context.Database.Migrate();
+
+            if (!context.HairSalons.Any(x => x.Name == "Amar"))
+            {
+                context.HairSalons.Add(new HairSalon() { Name = "Amar" , Address = "mejdan", Description = "Pravo dobars"});
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
