15d0663b1bfa29e9de01ed40bf8ec8467294031c
Amar Čerim <amarcerim8@gmail.com>
2021-09-22 15:47:59 +0200

    added hairsaloncity to model

---
+++ eFrizer/eFrizer.Model/HairSalonCity/HairSalonCity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eFrizer.Model.HairSalonCity
+{
+    public partial class HairSalonCity
+    {
+        public int HairSalonId { get; set; }
+        public int CityId { get; set; }
+
+        public virtual City City { get; set; }
+        public virtual HairSalon HairSalon { get; set; }
+    }
+}
---
+++ eFrizer/eFrizer.Model/HairSalonCity/HairSalonCityInsertRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eFrizer.Model.HairSalonCity
+{
+    class HairSalonCityInsertRequest
+    {
+        public int HairSalonId { get; set; }
+        public int CityId { get; set; }
+    }
+}
