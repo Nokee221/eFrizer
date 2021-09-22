6cf3dc2c3fd83b4c0694ec148945d44c0e83e367
Amar Čerim <amarcerim8@gmail.com>
2021-09-22 14:28:07 +0200

    added seed for roles

--- eFrizer/eFrizer/SetupService.cs
+++ eFrizer/eFrizer/SetupService.cs
@@ -18,6 +18,26 @@ namespace eFrizer
                 context.HairSalons.Add(new HairSalon() { Name = "Amar" , Address = "mejdan", Description = "Pravo dobars"});
             }
 
+            if(!context.Roles.Any(x => x.Name == "Administrator"))
+            {
+                context.Roles.Add(new Role() { Name = "Administrator" });
+            }
+
+            if (!context.Roles.Any(x => x.Name == "Manager"))
+            {
+                context.Roles.Add(new Role() { Name = "Manager" });
+            }
+
+            if (!context.Roles.Any(x => x.Name == "Client"))
+            {
+                context.Roles.Add(new Role() { Name = "Client" });
+            }
+
+            if (!context.Roles.Any(x => x.Name == "HairDresser"))
+            {
+                context.Roles.Add(new Role() { Name = "HairDresser" });
+            }
+
             context.SaveChanges();
         }
     }
