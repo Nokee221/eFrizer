5b9bb4b45383c07287ba87cbc4de4c6af28f8ee7
Amar Čerim <amarcerim8@gmail.com>
2021-09-22 14:49:39 +0200

    added applicationuser and applicationuserrole seed

--- eFrizer/eFrizer/SetupService.cs
+++ eFrizer/eFrizer/SetupService.cs
@@ -38,6 +38,64 @@ namespace eFrizer
                 context.Roles.Add(new Role() { Name = "HairDresser" });
             }
 
+            if(!context.ApplicationUsers.Any(x => x.Name == "User A"))
+            {
+                context.ApplicationUsers.Add(new ApplicationUser() { Name = "User A", Surname = "A User", Username = "auser" });
+            }
+
+            if (!context.ApplicationUsers.Any(x => x.Name == "User M"))
+            {
+                context.ApplicationUsers.Add(new ApplicationUser() { Name = "User M", Surname = "M User", Username = "muser" });
+            }
+
+            if (!context.ApplicationUsers.Any(x => x.Name == "User H"))
+            {
+                context.ApplicationUsers.Add(new ApplicationUser() { Name = "User H", Surname = "H User", Username = "huser" });
+            }
+
+            if (!context.ApplicationUsers.Any(x => x.Name == "User C"))
+            {
+                context.ApplicationUsers.Add(new ApplicationUser() { Name = "User C", Surname = "C User", Username = "cuser" });
+            }
+
+            context.SaveChanges();
+
+            if(!context.ApplicationUserRoles.Any(x => x.ApplicationUser.Name == "User A" && x.Role.Name == "Administrator"))
+            {
+                context.ApplicationUserRoles.Add(new ApplicationUserRole()
+                {
+                    ApplicationUserId = context.ApplicationUsers.Where(x => x.Name == "User A").First().ApplicationUserId,
+                    RoleId = context.Roles.Where(x => x.Name == "Administrator").First().RoleId
+                });
+            }
+
+            if (!context.ApplicationUserRoles.Any(x => x.ApplicationUser.Name == "User M" && x.Role.Name == "Manager"))
+            {
+                context.ApplicationUserRoles.Add(new ApplicationUserRole()
+                {
+                    ApplicationUserId = context.ApplicationUsers.Where(x => x.Name == "User M").First().ApplicationUserId,
+                    RoleId = context.Roles.Where(x => x.Name == "Manager").First().RoleId
+                });
+            }
+
+            if (!context.ApplicationUserRoles.Any(x => x.ApplicationUser.Name == "User H" && x.Role.Name == "HairDresser"))
+            {
+                context.ApplicationUserRoles.Add(new ApplicationUserRole()
+                {
+                    ApplicationUserId = context.ApplicationUsers.Where(x => x.Name == "User H").First().ApplicationUserId,
+                    RoleId = context.Roles.Where(x => x.Name == "HairDresser").First().RoleId
+                });
+            }
+
+            if (!context.ApplicationUserRoles.Any(x => x.ApplicationUser.Name == "User C" && x.Role.Name == "Client"))
+            {
+                context.ApplicationUserRoles.Add(new ApplicationUserRole()
+                {
+                    ApplicationUserId = context.ApplicationUsers.Where(x => x.Name == "User C").First().ApplicationUserId,
+                    RoleId = context.Roles.Where(x => x.Name == "Client").First().RoleId
+                });
+            }
+
             context.SaveChanges();
         }
     }
