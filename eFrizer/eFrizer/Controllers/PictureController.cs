d4870ea344d01d5a4d4b064eb823f691e4a3e679
Amar Čerim <amarcerim8@gmail.com>
2021-09-26 21:52:27 +0200

    added test endpoint picture setting in controller

---
+++ eFrizer/eFrizer/Controllers/PictureController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eFrizer.Controllers
+{
+    public class PictureController : ControllerBase
+    {
+        private IWebHostEnvironment hostingEnvironment;
+
+        public PictureController(IWebHostEnvironment environment)
+        {
+            hostingEnvironment = environment;
+        }
+
+        [HttpPost("UploadFile")]
+        public async Task<string> UploadFile(IFormFile file)
+        {
+            string imageName = new String(Path.GetFileNameWithoutExtension(file.FileName).Take(10).ToArray()).Replace(' ', '-');
+            imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(file.FileName);
+            string path = Path.Combine(hostingEnvironment.ContentRootPath, "Images/" + imageName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return imageName;
+        }
+    }
+}
