4a3a41d45508e664d7857f311a8124005b183b0b
Amar Čerim <amarcerim8@gmail.com>
2021-09-26 22:11:09 +0200

    added picture model

---
+++ eFrizer/eFrizer.Model/Picture/Picture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eFrizer.Model
+{
+    public class Picture
+    {
+        public int PictureId { get; set; }
+
+        public string Path { get; set; }7
+
+    }
+}
---
+++ eFrizer/eFrizer.Model/Picture/PictureInsertRequest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eFrizer.Model
+{
+    class PictureInsertRequest
+    {
+        public IFormFile ImageFile { get; set; }
+    }
+}
