b72b1446a3d5e02a0ae13d9c02daf72d9df29c6a
Amar Čerim <amarcerim8@gmail.com>
2021-10-14 21:17:40 +0200

    [Win] added forms for employee management , fetched employee data partially

---
+++ eFrizer/eFrizer.Win/frmEmployeeManager.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace eFrizer.Win
+{
+    partial class frmEmployeeManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvEmployees = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbStatus = new System.Windows.Forms.ComboBox();
+            this.btnAddManager = new System.Windows.Forms.Button();
+            this.btnAddHairDresser = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvEmployees);
+            this.groupBox1.Location = new System.Drawing.Point(12, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(609, 215);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Employee list";
+            // 
+            // dgvEmployees
+            // 
+            this.dgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployees.Location = new System.Drawing.Point(6, 22);
+            this.dgvEmployees.Name = "dgvEmployees";
+            this.dgvEmployees.RowTemplate.Height = 25;
+            this.dgvEmployees.Size = new System.Drawing.Size(597, 184);
+            this.dgvEmployees.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbStatus);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.cbType);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtName);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(609, 49);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Filter by";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(60, 19);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(167, 23);
+            this.txtName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(233, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Type";
+            // 
+            // cbType
+            // 
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Location = new System.Drawing.Point(270, 19);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(128, 23);
+            this.cbType.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(414, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Status";
+            // 
+            // cbStatus
+            // 
+            this.cbStatus.FormattingEnabled = true;
+            this.cbStatus.Location = new System.Drawing.Point(459, 19);
+            this.cbStatus.Name = "cbStatus";
+            this.cbStatus.Size = new System.Drawing.Size(144, 23);
+            this.cbStatus.TabIndex = 6;
+            // 
+            // btnAddManager
+            // 
+            this.btnAddManager.Location = new System.Drawing.Point(18, 298);
+            this.btnAddManager.Name = "btnAddManager";
+            this.btnAddManager.Size = new System.Drawing.Size(138, 23);
+            this.btnAddManager.TabIndex = 1;
+            this.btnAddManager.Text = "Add new manager";
+            this.btnAddManager.UseVisualStyleBackColor = true;
+            // 
+            // btnAddHairDresser
+            // 
+            this.btnAddHairDresser.Location = new System.Drawing.Point(181, 298);
+            this.btnAddHairDresser.Name = "btnAddHairDresser";
+            this.btnAddHairDresser.Size = new System.Drawing.Size(138, 23);
+            this.btnAddHairDresser.TabIndex = 4;
+            this.btnAddHairDresser.Text = "Add new hair dresser";
+            this.btnAddHairDresser.UseVisualStyleBackColor = true;
+            // 
+            // frmEmployeeManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 333);
+            this.Controls.Add(this.btnAddHairDresser);
+            this.Controls.Add(this.btnAddManager);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmEmployeeManager";
+            this.Text = "frmEmployeeManager";
+            this.Load += new System.EventHandler(this.frmEmployeeManager_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvEmployees;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cbStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnAddManager;
+        private System.Windows.Forms.Button btnAddHairDresser;
+    }
+}
---
+++ eFrizer/eFrizer.Win/frmEmployeeManager.cs
@@ -0,0 +1,82 @@
+﻿using eFrizer.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eFrizer.Win
+{
+    public partial class frmEmployeeManager : Form
+    {
+        //TODO: Should these services be actually querying an Employees table?
+        //Or maybe should I make a controller that returns the Employees with a given Hair Salon Id?
+        private HairSalon _hairSalon;
+        private APIService _hairDressers = new APIService("HairSalonHairDresser");
+        private APIService _managers = new APIService("HairSalonManager");
+
+        public frmEmployeeManager(HairSalon hairSalon)
+        {
+            _hairSalon = hairSalon;
+            InitializeComponent();
+        }
+
+        private async void frmEmployeeManager_Load(object sender, EventArgs e)
+        {
+            await LoadData();
+        }
+
+        private async Task LoadData()
+        {
+            await LoadEmployeeList();
+        }
+
+        private async Task LoadEmployeeList()
+        {
+            var reqHD = new HairSalonHairDresserSearchRequest() { HairSalonId = _hairSalon.HairSalonId};
+            var reqM = new HairSalonManagerSearchRequest() { HairSalonId = _hairSalon.HairSalonId};
+
+            var hairDressers = await _hairDressers.Get<List<HairSalonHairDresser>>(reqHD);
+            var managers = await _managers.Get<List<HairSalonManager>>(reqM);
+
+            var dataTable = new DataTable();
+            dataTable.Columns.AddRange(new DataColumn[]
+            {
+                new DataColumn("Name"),
+                new DataColumn("Surname"),
+                new DataColumn("Description"),
+                new DataColumn("Type"),
+                new DataColumn("Status")
+            });
+
+            foreach (var item in hairDressers)
+            {
+                var row = dataTable.NewRow();
+                row["Name"] = item.HairDresser.Name;
+                row["Surname"] = item.HairDresser.Surname;
+                row["Description"] = item.HairDresser.Description;
+                row["Type"] = item.GetType();
+                //row["Status"] = item.Status,
+                dataTable.Rows.Add(row);
+            }
+
+            //foreach (var item in managers)
+            //{
+            //    var row = dataTable.NewRow();
+            //    row["Name"] = item.Manager.Name;
+            //    row["Surname"] = item.Manager.Surname;
+            //    row["Description"] = item.Manager.Description;
+            //    row["Type"] = item.GetType();
+            //    //row["Status"] = item.Status,
+            //    dataTable.Rows.Add(row);
+            //}
+
+            dgvEmployees.DataSource = dataTable;
+            
+        }
+    }
+}
