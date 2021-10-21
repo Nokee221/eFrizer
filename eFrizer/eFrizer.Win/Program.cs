b6bc8adf55a8cac2619e9cbca5570358bcc00c8f
Amar Čerim <amarcerim8@gmail.com>
2021-10-21 22:58:46 +0200

    added powershell script to main method of winforms to install cert

--- eFrizer/eFrizer.Win/Program.cs
+++ eFrizer/eFrizer.Win/Program.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Automation;
+using System.Net;
+using System.Security;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,10 +17,28 @@ namespace eFrizer.Win
         [STAThread]
         static void Main()
         {
+            SecureString theSecureString = new NetworkCredential("", "1234").SecurePassword;
+            PowerShell ps = PowerShell.Create();
+            ps.Streams.Warning.DataAdded += PowerShellOutputEventHandler;
+            ps.AddCommand("Import-PfxCertificate")
+                .AddParameter("FilePath","../../../../efrizer_cert.pfx")
+                .AddParameter("CertStoreLocation", "cert:\\CurrentUser\\Root")
+                .AddParameter("Password", theSecureString);
+            ps.AddCommand("Write-Warning");
+            //ps.AddCommand("Import-Certificate")
+            //    .AddParameter("FilePath","efrizer_cert.pfx")  
+            //    .AddParameter("CertStoreLocation", "Cert:\\LocalMachine\\Root");
+            ps.Invoke();
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new InitScreen());
+        }
+
+        private static void PowerShellOutputEventHandler(object sender, DataAddedEventArgs e)
+        {
+            WarningRecord newRecord = ((PSDataCollection<WarningRecord>)sender)[e.Index];
+            MessageBox.Show(newRecord.Message);
         }
     }
 }
