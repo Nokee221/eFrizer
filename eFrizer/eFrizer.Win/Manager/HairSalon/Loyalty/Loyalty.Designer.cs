a640f8decfa15c49254e7d1b37f4a7e6e1eedb2f
Amar Čerim <amarcerim8@gmail.com>
2021-11-10 13:39:37 +0100

    [Win] Added screen for loyalty system definition

---
+++ eFrizer/eFrizer.Win/Manager/HairSalon/Loyalty/Loyalty.Designer.cs
@@ -0,0 +1,291 @@
+﻿
+namespace eFrizer.Win
+{
+    partial class Loyalty
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbService = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvLoyalty = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.Name = new System.Windows.Forms.Label();
+            this.cmbBonusType = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDiscount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbExpiration = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rtbDescription = new System.Windows.Forms.RichTextBox();
+            this.btnSaveType = new System.Windows.Forms.Button();
+            this.btnSaveBonus = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLoyalty)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmbService
+            // 
+            this.cmbService.FormattingEnabled = true;
+            this.cmbService.Location = new System.Drawing.Point(69, 32);
+            this.cmbService.Name = "cmbService";
+            this.cmbService.Size = new System.Drawing.Size(121, 23);
+            this.cmbService.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Service:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvLoyalty);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 192);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Defined loyalty bonuses";
+            // 
+            // dgvLoyalty
+            // 
+            this.dgvLoyalty.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLoyalty.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvLoyalty.Location = new System.Drawing.Point(3, 19);
+            this.dgvLoyalty.Name = "dgvLoyalty";
+            this.dgvLoyalty.RowTemplate.Height = 25;
+            this.dgvLoyalty.Size = new System.Drawing.Size(770, 170);
+            this.dgvLoyalty.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSaveBonus);
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.cmbBonusType);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.cmbService);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 210);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 228);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Add new loyalty bonus";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnSaveType);
+            this.groupBox3.Controls.Add(this.rtbDescription);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.cmbExpiration);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.txtDiscount);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.txtName);
+            this.groupBox3.Controls.Add(this.Name);
+            this.groupBox3.Location = new System.Drawing.Point(0, 61);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(776, 167);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Define bonus type";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(61, 25);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(191, 23);
+            this.txtName.TabIndex = 2;
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(16, 28);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(39, 15);
+            this.Name.TabIndex = 1;
+            this.Name.Text = "Name";
+            // 
+            // cmbBonusType
+            // 
+            this.cmbBonusType.FormattingEnabled = true;
+            this.cmbBonusType.Location = new System.Drawing.Point(280, 32);
+            this.cmbBonusType.Name = "cmbBonusType";
+            this.cmbBonusType.Size = new System.Drawing.Size(109, 23);
+            this.cmbBonusType.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(208, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Bonus type";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(479, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(109, 23);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(398, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Activates on:";
+            // 
+            // txtDiscount
+            // 
+            this.txtDiscount.Location = new System.Drawing.Point(61, 64);
+            this.txtDiscount.Name = "txtDiscount";
+            this.txtDiscount.Size = new System.Drawing.Size(191, 23);
+            this.txtDiscount.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Discount";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(2, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Expires in";
+            // 
+            // cmbExpiration
+            // 
+            this.cmbExpiration.FormattingEnabled = true;
+            this.cmbExpiration.Location = new System.Drawing.Point(61, 106);
+            this.cmbExpiration.Name = "cmbExpiration";
+            this.cmbExpiration.Size = new System.Drawing.Size(100, 23);
+            this.cmbExpiration.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(279, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Description";
+            // 
+            // rtbDescription
+            // 
+            this.rtbDescription.Location = new System.Drawing.Point(352, 25);
+            this.rtbDescription.Name = "rtbDescription";
+            this.rtbDescription.Size = new System.Drawing.Size(406, 104);
+            this.rtbDescription.TabIndex = 8;
+            this.rtbDescription.Text = "";
+            // 
+            // btnSaveType
+            // 
+            this.btnSaveType.Location = new System.Drawing.Point(604, 138);
+            this.btnSaveType.Name = "btnSaveType";
+            this.btnSaveType.Size = new System.Drawing.Size(155, 23);
+            this.btnSaveType.TabIndex = 9;
+            this.btnSaveType.Text = "Save type";
+            this.btnSaveType.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveBonus
+            // 
+            this.btnSaveBonus.Location = new System.Drawing.Point(604, 32);
+            this.btnSaveBonus.Name = "btnSaveBonus";
+            this.btnSaveBonus.Size = new System.Drawing.Size(155, 23);
+            this.btnSaveBonus.TabIndex = 10;
+            this.btnSaveBonus.Text = "Save bonus";
+            this.btnSaveBonus.UseVisualStyleBackColor = true;
+            // 
+            // Loyalty
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Loyalty";
+            this.Text = "Loyalty";
+            this.Load += new System.EventHandler(this.Loyalty_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLoyalty)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbService;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvLoyalty;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.ComboBox cmbBonusType;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDiscount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSaveBonus;
+        private System.Windows.Forms.Button btnSaveType;
+        private System.Windows.Forms.RichTextBox rtbDescription;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbExpiration;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ eFrizer/eFrizer.Win/Manager/HairSalon/Loyalty/Loyalty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace eFrizer.Win
+{
+    public partial class Loyalty : Form
+    {
+        public Loyalty()
+        {
+            InitializeComponent();
+        }
+
+        private void Loyalty_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
