d7371a95b50dc1aeb6243ff8790dc4c471a0dbce
Amar Čerim <amarcerim8@gmail.com>
2021-11-16 01:15:55 +0100

    [Win] changed frmLoyalty design and displayed available services in create new bonus form

--- eFrizer/eFrizer.Win/Manager/HairSalon/Loyalty/frmLoyalty.cs
+++ eFrizer/eFrizer.Win/Manager/HairSalon/Loyalty/frmLoyalty.cs
@@ -1,18 +1,32 @@
-﻿using System;
+﻿using eFrizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace eFrizer.Win
 {
     public partial class frmLoyalty : Form
     {
+        private APIService _bonuses = new APIService("HairSalonServiceLoyaltyBonus");
+        private APIService _services = new APIService("HairSalonService");
+
         public frmLoyalty()
         {
             InitializeComponent();
         }
 
-        private void Loyalty_Load(object sender, EventArgs e)
+        private async void Loyalty_Load(object sender, EventArgs e)
         {
+            await LoadHairSalonServices();
+        }
 
+        private async Task LoadHairSalonServices()
+        {
+            var services = await _services.Get<List<HairSalonService>>();
+            cmbService.DataSource = services;
+            cmbService.DisplayMember = "ServiceName";
+            cmbService.ValueMember = "Id";
         }
     }
 }
