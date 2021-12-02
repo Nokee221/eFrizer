db4e333610a060978a4bed66cf837ce37175e5f1
Amar Čerim <amarcerim8@gmail.com>
2021-12-02 01:23:32 +0100

    [Flutter] formatting fix

--- eFrizer/flutter_efrizer/lib/pages/edit_profile_page.dart
+++ eFrizer/flutter_efrizer/lib/pages/edit_profile_page.dart
@@ -6,8 +6,6 @@ import 'package:flutter_login/services/api_service.dart';
 import 'package:flutter_login/widget/button_widget.dart';
 import 'package:flutter_login/widget/profile_widget.dart';
 
-
-
 class EditeProfilePage extends StatefulWidget {
   final ApplicationUser user;
   const EditeProfilePage(this.user, {Key? key}) : super(key: key);
@@ -49,114 +47,115 @@ class _EditeProfilePageState extends State<EditeProfilePage> {
 
   var result = null;
   Future<void> PutData() async {
-    result = await APIService.updateUser('ApplicationUser', usernameController.text, nameController.text, surnameController.text, descriptionController.text);
+    result = await APIService.updateUser(
+        'ApplicationUser',
+        usernameController.text,
+        nameController.text,
+        surnameController.text,
+        descriptionController.text);
   }
 
   @override
   Widget build(BuildContext context) => Builder(
-    builder: (context) => Scaffold(
-      appBar:  AppBar(
-        leading: BackButton(color: Colors.blue),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        actions: [
-        IconButton(
-        icon: Icon(icon),
-        color: Colors.blue,
-        onPressed: () {},
-      ),
-      ],
-     ),
-     body: ListView(
-       padding: EdgeInsets.symmetric(horizontal: 32),
-       physics: BouncingScrollPhysics(),
-       children: [
-         ProfileWidget(
-            isEdit: true,
-            imagePath: 'https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo=',
-            onClicked: ()async{},
-         ),
-         const SizedBox(height: 24),
-         Column(
-           crossAxisAlignment: CrossAxisAlignment.start,
-           children: [
-             Text(
-               "Username",
-               style: TextStyle(fontWeight: FontWeight.bold , fontSize: 16),
-             ),
-             const SizedBox(height: 8),
-             TextField(
-               controller: usernameController,
-               decoration: InputDecoration(
-                 border: OutlineInputBorder(
-                   borderRadius: BorderRadius.circular(12),
-                 )
-               ),
-              maxLines: 1,
-             ),
-             const SizedBox(height: 8),
-             Text(
-               "Name",
-               style: TextStyle(fontWeight: FontWeight.bold , fontSize: 16),
-             ),
-             const SizedBox(height: 8),
-             TextField(
-               controller: nameController,
-               decoration: InputDecoration(
-                 border: OutlineInputBorder(
-                   borderRadius: BorderRadius.circular(12),
-                 )
-               ),
-              maxLines: 1,
-             ),
-             const SizedBox(height: 8),
-             Text(
-               "Surname",
-               style: TextStyle(fontWeight: FontWeight.bold , fontSize: 16),
-             ),
-             const SizedBox(height: 8),
-             TextField(
-               controller: surnameController,
-               decoration: InputDecoration(
-                 border: OutlineInputBorder(
-                   borderRadius: BorderRadius.circular(12),
-                 )
-               ),
-              maxLines: 1,
-             ),
-             const SizedBox(height: 8),
-             Text(
-               "About",
-               style: TextStyle(fontWeight: FontWeight.bold , fontSize: 16),
-             ),
-             const SizedBox(height: 8),
-             TextField(
-               controller: descriptionController,
-              decoration: InputDecoration(
-                 border: OutlineInputBorder(
-                   borderRadius: BorderRadius.circular(12),
-                 )
-               ),
-              maxLines: 10,
-             ),
-             const SizedBox(height: 8),
-             ButtonWidget(
-               text: "Save",
-               onClicked: () async{
-                 await PutData();
-                 if(result != null)
-                 {
-                   Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ProfilePage(user)));
-                 }
-               },
-             )
-           ],
-         )
-       ],
-     ),
-    ),
-  );
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            leading: BackButton(color: Colors.blue),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            actions: [
+              IconButton(
+                icon: Icon(icon),
+                color: Colors.blue,
+                onPressed: () {},
+              ),
+            ],
+          ),
+          body: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 32),
+            physics: BouncingScrollPhysics(),
+            children: [
+              ProfileWidget(
+                isEdit: true,
+                imagePath:
+                    'https://media.istockphoto.com/photos/millennial-male-team-leader-organize-virtual-workshop-with-employees-picture-id1300972574?b=1&k=20&m=1300972574&s=170667a&w=0&h=2nBGC7tr0kWIU8zRQ3dMg-C5JLo9H2sNUuDjQ5mlYfo=',
+                onClicked: () async {},
+              ),
+              const SizedBox(height: 24),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Username",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  const SizedBox(height: 8),
+                  TextField(
+                    controller: usernameController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    )),
+                    maxLines: 1,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    "Name",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  const SizedBox(height: 8),
+                  TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    )),
+                    maxLines: 1,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    "Surname",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  const SizedBox(height: 8),
+                  TextField(
+                    controller: surnameController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    )),
+                    maxLines: 1,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    "About",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  const SizedBox(height: 8),
+                  TextField(
+                    controller: descriptionController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    )),
+                    maxLines: 10,
+                  ),
+                  const SizedBox(height: 8),
+                  ButtonWidget(
+                    text: "Save",
+                    onClicked: () async {
+                      await PutData();
+                      if (result != null) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProfilePage(user)));
+                      }
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      );
 }
