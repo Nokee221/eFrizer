45057f643ebc1dc3c901076ce8efacb758cf4d5a
Kenan Cosic <45003119+Nokee221@users.noreply.github.com>
2022-01-11 21:45:21 +0100

    Update HairSalon.dart

--- eFrizer/flutter_efrizer/lib/models/HairSalon.dart
+++ eFrizer/flutter_efrizer/lib/models/HairSalon.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
-
 import 'package:json_annotation/json_annotation.dart';
 
 part 'HairSalon.g.dart';
@@ -20,26 +19,6 @@ class HairSalon{
     required this.Address,
     required this.CityId
   });
-
-  /*factory HairSalon.fromJson(Map<String,dynamic> json)
-  {
-    return HairSalon(
-      HairSalonId: int.parse(json["hairSalonId"].toString()),
-      Name: json["name"],
-      Description: json["description"],
-      Address: json["address"],
-      CityId: int.parse(json["cityId"].toString())
-    );
-  }
-
-
-  Map<String,dynamic> toJson() =>{
-    "hairSalonId": HairSalonId,
-    "name":Name,
-    "description": Description,
-    "address": Address,
-    "cityId": CityId,
-  };*/
 
   factory HairSalon.fromJson(Map<String,dynamic> data) => _$HairSalonFromJson(data);
 
