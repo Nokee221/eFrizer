9be00c3f6ef310277f230dea49932a66c412eabe
Amar Čerim <amarcerim8@gmail.com>
2022-01-13 12:59:01 +0100

    [Win] added sum to report and limited the data displayed on reports

--- eFrizer/eFrizer.Win/Manager/HairSalon/Reports/ReservationReport.Designer.cs
+++ eFrizer/eFrizer.Win/Manager/HairSalon/Reports/ReservationReport.Designer.cs
@@ -30,11 +30,15 @@ namespace eFrizer.Win
         private void InitializeComponent()
         {
             this.btnReport = new System.Windows.Forms.Button();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnReport
             // 
-            this.btnReport.Location = new System.Drawing.Point(43, 31);
+            this.btnReport.Location = new System.Drawing.Point(558, 24);
             this.btnReport.Name = "btnReport";
             this.btnReport.Size = new System.Drawing.Size(218, 23);
             this.btnReport.TabIndex = 0;
@@ -42,21 +46,64 @@ namespace eFrizer.Win
             this.btnReport.UseVisualStyleBackColor = true;
             this.btnReport.Click += new System.EventHandler(this.btnReport_Click);
             // 
+            // dtpFrom
+            // 
+            this.dtpFrom.Location = new System.Drawing.Point(73, 24);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(200, 23);
+            this.dtpFrom.TabIndex = 1;
+            this.dtpFrom.ValueChanged += new System.EventHandler(this.dtpFrom_ValueChanged);
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.Location = new System.Drawing.Point(323, 24);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(200, 23);
+            this.dtpTo.TabIndex = 2;
+            this.dtpTo.ValueChanged += new System.EventHandler(this.dtpTo_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(295, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "To:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "From:";
+            // 
             // ReservationReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpTo);
+            this.Controls.Add(this.dtpFrom);
             this.Controls.Add(this.btnReport);
             this.Name = "ReservationReport";
             this.Text = "ReservationReport";
             this.Load += new System.EventHandler(this.ReservationReport_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnReport;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- eFrizer/eFrizer.Win/Manager/HairSalon/Reports/ReservationReport.cs
+++ eFrizer/eFrizer.Win/Manager/HairSalon/Reports/ReservationReport.cs
@@ -1,13 +1,12 @@
 ﻿using DoddleReport;
+using eFrizer.Model;
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,37 +15,69 @@ namespace eFrizer.Win
     public partial class ReservationReport : Form
     {
         private APIService _reservations = new APIService("Reservation");
+        private List<Model.Reservation> reservationData { get; set; }
         private Report reservationReport;
+
 
         public ReservationReport()
         {
             InitializeComponent();
         }
 
+        class ReservationData
+        {
+            public string Client { get; set; }
+            public string HairDresser { get; set; }
+            public string Service { get; set; }
+            public float Price { get; set; }
+        }
+
         private async void ReservationReport_Load(object sender, EventArgs e)
         {
-            var reservations = await GetReservations();
-            reservationReport = new Report(reservations.ToReportSource(), new DoddleReport.iTextSharp.PdfReportWriter());
-
-            reservationReport.TextFields.Title = "Reservation Report";
-            reservationReport.TextFields.SubTitle = "Financial reports of hair salon";
-            reservationReport.TextFields.Footer = "Copyright 2021 &copy; Kenan&Amar";
-            reservationReport.TextFields.Header = string.Format(@"
-            Report Generated: {0}
-            Total Reservations: {1}
-            ", DateTime.Now, reservations.Count());
-            
+            reservationData = await GetReservations();
         }
 
         public async Task<List<Model.Reservation>> GetReservations()
         {
-            var reservations = await _reservations.Get<List<Model.Reservation>>();
+            var reservationSearchRequest = new ReservationSearchRequest()
+            {
+                From = dtpFrom.Value,
+                To = dtpTo.Value
+            };
+            var reservations = await _reservations.Get<List<Model.Reservation>>(reservationSearchRequest);
             return reservations;
         }
 
         private void btnReport_Click(object sender, EventArgs e)
         {
-            using(var stream = File.Create("ReservationReport.pdf"))
+            var data = reservationData.Select(x =>
+            new ReservationData
+            {
+                Client = x.Client.Name,
+                HairDresser = x.HairDresser.Name,
+                Service = x.ServiceName,
+                Price = x.HairSalonService.Price
+            });
+
+            float sum = 0.0f;
+            foreach (var item in data)
+            {
+                sum += item.Price;
+            }
+
+
+            reservationReport = new Report(data.ToReportSource(), new DoddleReport.iTextSharp.PdfReportWriter());
+
+            reservationReport.TextFields.Title = "Financial report for <HairSalonName>";//TODO: reference the current hair salon
+            reservationReport.TextFields.SubTitle = "The report is based on data from the last 5 days.";
+            reservationReport.TextFields.SubTitle = string.Format(@"Sum of column 'Price': {0}", sum);
+            reservationReport.TextFields.Footer = "Copyright 2021 &copy; Kenan&Amar";
+            reservationReport.TextFields.Header = string.Format(@"
+            Report Generated: {0}
+            Total Reservations: {1}
+            ", DateTime.Now, data.Count());
+
+            using (var stream = File.Create("ReservationReport.pdf"))
             {
                 reservationReport.WriteReport(stream);
             }
@@ -58,6 +89,16 @@ namespace eFrizer.Win
             };
             p.Start();
 
+        }
+
+        private async void dtpFrom_ValueChanged(object sender, EventArgs e)
+        {
+            reservationData = await GetReservations();
+        }
+
+        private async void dtpTo_ValueChanged(object sender, EventArgs e)
+        {
+            reservationData = await GetReservations();
         }
     }
 }
