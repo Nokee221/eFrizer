5f40f9812ceac441b99ca721b7abffb7792404d8
Amar Čerim <amarcerim8@gmail.com>
2022-01-14 14:25:50 +0100

    [Flutter] updated reservation property names accordingly

--- eFrizer/flutter_efrizer/lib/models/reservation/reservation.dart
+++ eFrizer/flutter_efrizer/lib/models/reservation/reservation.dart
@@ -2,42 +2,35 @@ import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 
-class Reservation{
+class Reservation {
   final int HairDresserId;
   final int? ClientId;
-  final int ServiceId;
+  final int HairSalonServiceId;
   final DateTime From;
   final DateTime To;
 
   final String HairDresserName;
   final String ServiceName;
 
-
-  Reservation({
-    required this.ClientId,
-    required this.HairDresserId,
-    required this.ServiceId,
-    required this.From,
-    required this.To,
-    required this.HairDresserName,
-    required this.ServiceName
-  });
-
- 
+  Reservation(
+      {required this.ClientId,
+      required this.HairDresserId,
+      required this.HairSalonServiceId,
+      required this.From,
+      required this.To,
+      required this.HairDresserName,
+      required this.ServiceName});
 
   factory Reservation.fromJson(Map<String, dynamic> json) {
     return Reservation(
-      HairDresserId: int.parse(json['hairDresserId'].toString()),
-      ClientId: int.parse(json['clientId'].toString()),
-      ServiceId: int.parse(json['serviceId'].toString()),
-      From: DateTime.parse(json['from']),
-      To: DateTime.parse(json['to']),
-      HairDresserName: json['hairDresserName'],
-      ServiceName: json['serviceName']
-    );
+        HairDresserId: int.parse(json['hairDresserId'].toString()),
+        ClientId: int.parse(json['clientId'].toString()),
+        HairSalonServiceId: int.parse(json['hairSalonServiceId'].toString()),
+        From: DateTime.parse(json['from']),
+        To: DateTime.parse(json['to']),
+        HairDresserName: json['hairDresserName'],
+        ServiceName: json['serviceName']);
   }
 
-  Map<String, dynamic> toJson() => {
-        
-      };
+  Map<String, dynamic> toJson() => {};
 }
