d50cea68fb1ffeb9acf82afefcccfafadd1fdd60
Amar Čerim <amarcerim8@gmail.com>
2022-01-15 15:24:05 +0100

    [Win+eFrizer] hotfixes

--- eFrizer/flutter_efrizer/lib/pages/hair_dresser_home_page.dart
+++ eFrizer/flutter_efrizer/lib/pages/hair_dresser_home_page.dart
@@ -2,7 +2,6 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login/models/application_user/application_user.dart';
 import 'package:flutter_login/pages/hair_dresser_calendar_page.dart';
-import 'package:flutter_login/pages/hair_dresser_home_page_2.dart';
 import 'package:flutter_login/pages/hair_dresser_profile_page.dart';
 import 'profile_page.dart';
 
